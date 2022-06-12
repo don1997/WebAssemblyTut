77382c2a88f36873c379979559c7a2e8fed8ecf1
don1997 <donald_j97@hotmail.com>
2022-06-12 18:39:36 -0400

    first commit

---
+++ bindings.cpp
@@ -0,0 +1,13 @@
+#include <emscripten/bind.h>
+#include "newtonraphson.hpp"
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(newtonraphson) {
+   class_<NewtonRaphson>("NewtonRaphson")
+      .constructor<float>()
+      .function("solve", &NewtonRaphson::solve)
+      ;
+}
+
+
